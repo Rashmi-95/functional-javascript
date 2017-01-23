cbdae4880c3ea4dc5f0d51f0372f7ec38bfbe0a8
rashmi-95 <rashmi05rash@gmail.com>
2017-01-23 15:11:34 +0530

    .

--- call.js
+++ call.js
@@ -1,7 +1,10 @@
 function duckCount() {
-    var argumentsWihQuack = Array.prototype.filter.call(arguments,function isArgumentDuck(argument) {
-         return Object.prototype.hasOwnProperty.call(argument,'quack');
-     });
-     return  argumentsWihQuack.length;
+    var arrayOfKeys = Object.keys(arguments); // find keys i.e 0,1,2 - useless but used in filter
+    originalInputArgument = arguments; // we copy the arguments so that it an be used inside filter for using arguments[keys]
+    var arrayOfDucks = arrayOfKeys.filter( function isArgumentDuck(eachKey) {
+        var value = originalInputArgument[eachKey]; // we get the value for each key using the copied argument and key
+        return {}.hasOwnProperty.call(value,'quack'); // return if its true
+    });
+    return  arrayOfDucks.length;
 }
 module.exports = duckCount
